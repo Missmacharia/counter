80dc8358590290622870d4c8b00e2181b1a598c1
Missmacharia <99560116+Missmacharia@users.noreply.github.com>
2022-11-03 08:52:43 +0300

    Update Increment.jsx

--- src/components/increment/Increment.jsx
+++ src/components/increment/Increment.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
 import { useEffect } from 'react'
 const Increment = ({setCount}) => {
-    // const handleIncrementClick=()=>{
-    //     setCount(prev=>prev+1)
-    // }
-    let interval;
-    const handleIncrementClick=(()=>{
-       interval= setInterval(()=>{
-            setCount((prev)=>prev +1)
-        },1000)
-    })
-    useEffect(()=>{
-        handleIncrementClick()
-        return()=>clearInterval(interval)
-    },[] )
+    
   return (
     <div>
-      {/* <button count={count} >Increase</button> */}
+      <h1>Decrement</div>
     </div>
   )
 }
