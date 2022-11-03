fb0162caa78f36ac2bd942487545b8e324455a0d
Missmacharia <99560116+Missmacharia@users.noreply.github.com>
2022-11-03 08:51:27 +0300

    Update Decrement.jsx

--- src/components/decrement/Decrement.jsx
+++ src/components/decrement/Decrement.jsx
@@ -2,22 +2,6 @@ import React from "react";
 // import { useEffect } from "react";
 
 const Decrement =()=>{
-// ({ countDecrease, setCountDecrease }) => {
-  // const handleDecrementClick =()=>{
-  //     setCountDecrease(prev=>prev-1)
-  // }
-//   let interval;
-//   const handleDecrement = () => {
-//     interval = setInterval(() => {
-//       setCountDecrease((prev) => prev - 1);
-//     }, 1000);
-//   };
-
-//   useEffect(() => {
-//     console.log('from decrement');
-//     handleDecrement()
-// return ()=>clearInterval(interval)
-//   }, []);
   return (
     <div>
       <p>Adding</p>
